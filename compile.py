0346832bcbe9ced7793c2e36553af59f83cabb1b
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2019-11-29 22:34:34 +0300

    переработка

--- compile.py
+++ compile.py
@@ -3,21 +3,39 @@ import os
 f_in = './middle of the process/'
 f_out = './output/'
 
-
-def compile(f_in, f_out):
+def compile(f_in,f_out):
     f = open(f_in)
     file = open(f_out[:-4] + ".txt", 'w')
     txt = f.read()
     txt = txt.replace(",", "")
     file.write(txt)
-    print("File \"{}\" compiled".format(f_in[24:]))
+    print(f"File \"{f_in[24:]}\" compiled")
     f.close()
 
-
-if __name__ == "__main__":
+def running(f_in,f_out):
     files = os.listdir(f_in)
     for file in files:
         if file.endswith(".csv"):
             compile(f_in + file, f_out + file)
         else:
-            print("File \"{}\" have wrong extension".format(file))
+            print(f"File \"{file}\" have wrong extension")
+    else:
+        if len(files) == 0:
+            print(f"There is nothing to work in the folder \"{f_in[2:-1]}\"")
+
+if __name__ == "__main__":
+    try:
+        running(f_in,f_out)
+    except FileNotFoundError:
+        try:
+            from initialisation import func
+            print("There is no initialization in the folder, initialization will running now")
+            print("Running initialisation\n")
+            print("-"*50)
+            func()
+            print("-"*50,"\n")
+            running(f_in,f_out)
+        except ModuleNotFoundError:
+            print("There is no \"initialisation.py\" file in the folder, download it")
+
+    input('\nPress ENTER to exit')
--- decompile.py
+++ decompile.py
@@ -12,14 +12,33 @@ def decompile(f_in,f_out):
             file.write(txt[lit] + ",")
         else:
             file.write(txt[lit])
-    print("File \"{}\" decompiled".format(f_in[10:]))
+    print(f"File \"{f_in[10:]}\" decompiled")
     f.close()
 
-
-if __name__ == "__main__":
+def running(f_in,f_out):
     files = os.listdir(f_in)
     for file in files:
         if file.endswith(".txt"):
             decompile(f_in + file, f_out + file)
         else:
-            print("File \"{}\" have wrong extension".format(file))
+            print(f"File \"{file}\" have wrong extension")
+    else:
+        if len(files) == 0:
+            print(f"There is nothing to work in the folder \"{f_in[2:-1]}\"")
+
+if __name__ == "__main__":
+    try:
+        running(f_in,f_out)
+    except FileNotFoundError:
+        try:
+            from initialisation import func
+            print("There is no initialization in the folder, initialization will running now")
+            print("Running initialisation\n")
+            print("-"*50)
+            func()
+            print("-"*50,"\n")
+            running(f_in,f_out)
+        except ModuleNotFoundError:
+            print("There is no \"initialisation.py\" file in the folder, download it")
+
+    input('\nPress ENTER to exit')
